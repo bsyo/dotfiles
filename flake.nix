2764b4f08665c5e67173546dd5cbe42cde662d3a
bsyo <119839369+bsyo@users.noreply.github.com>
2025-01-30 14:35:03 +0900

    feat: update system packages to include neovim and tmux
    - Replace vim with neovim and add tmux to system packages
    - Enable Fish shell and set it as the default shell

--- flake.nix
+++ flake.nix
@@ -12,15 +12,19 @@
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ pkgs.vim
-        ];
+      environment.systemPackages = with pkgs; [
+        neovim
+        tmux
+      ];
+
+      # Enable Fish Shell
+      programs.fish.enable = true;
+      environment.shells = with pkgs; [ fish ];
+      # Set Fish as default shell for your user
+      environment.variables.SHELL = "${pkgs.fish}/bin/fish";
 
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
 
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
